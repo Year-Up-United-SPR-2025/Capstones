7cc23084640b8658a14555976727a1aeb1961ef0
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-05-23 12:37:50 -0400

    added the getters and price for the Heck_Drink_Sizes and Heck_Sandwich_Sizes.java

--- One_Heck_Of_A_Sandwich_Cap_2/src/main/java/com/pluralsight/Sandwich_Emuns/Heck_Drink_Sizes.java
+++ One_Heck_Of_A_Sandwich_Cap_2/src/main/java/com/pluralsight/Sandwich_Emuns/Heck_Drink_Sizes.java
@@ -1,7 +1,17 @@
 package com.pluralsight.Sandwich_Emuns;
 
 public enum Heck_Drink_Sizes {
-    SMALL,
-    MEDIUM,
-    LARGE
+    SMALL(2.00),
+    MEDIUM(2.50),
+    LARGE(3.00);
+
+    private final double price;
+
+    Heck_Drink_Sizes(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
--- One_Heck_Of_A_Sandwich_Cap_2/src/main/java/com/pluralsight/Sandwich_Emuns/Heck_Sandwich_Sizes.java
+++ One_Heck_Of_A_Sandwich_Cap_2/src/main/java/com/pluralsight/Sandwich_Emuns/Heck_Sandwich_Sizes.java
@@ -1,7 +1,17 @@
 package com.pluralsight.Sandwich_Emuns;
 
 public enum Heck_Sandwich_Sizes {
-    FOUR_INCH,
-    EIGHT_INCH,
-    TWELVE_INCH
+    FOUR_INCH(5.50),
+    EIGHT_INCH(7.00),
+    TWELVE_INCH(8.50);
+
+    private final double basePrice;
+
+    Heck_Sandwich_Sizes(double basePrice){
+        this.basePrice = basePrice;
+    }
+
+    public double getBasePrice(){
+        return basePrice;
+    }
 }
