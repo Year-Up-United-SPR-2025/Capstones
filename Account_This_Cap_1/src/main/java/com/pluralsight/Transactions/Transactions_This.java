2fdfe0e771ff89fdbbdca5b35bcaede9556ed781
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-06-02 09:47:17 -0400

    Added the classes and packages I'm using

--- Account_This_Cap_1/src/main/java/com/pluralsight/Transactions/Transactions_This.java
+++ Account_This_Cap_1/src/main/java/com/pluralsight/Transactions/Transactions_This.java
@@ -1,4 +1,47 @@
 package com.pluralsight.Transactions;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Transactions_This {
+    private LocalDate date;
+    private LocalTime time;
+    private String description;
+    private String vendor;
+    private double amount;
+
+    public Transactions_This(LocalDate date, LocalTime time, String description, String vendor, double amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    // Getters
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s | %-20s | %-15s | $%.2f",
+                date, time, description, vendor, amount);
+    }
 }
